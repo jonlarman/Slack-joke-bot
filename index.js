c7060f84a4377a72ca87c2091eab9bf7389d9ee5
Jon Larman <jlarman@ethosgroup.com>
2016-06-11 19:46:33 -0500

    maybe this will work

--- index.js
+++ index.js
@@ -51,11 +51,11 @@ controller.hears('auditbot', ['ambient'], function (bot, message) {
   bot.reply(message, 'AuditBot is always listening. And watching.')
 })
 
-controller.hears(['permissions', 'permission'], ['message_received'], function (bot, message) {
+controller.hears(['permissions', 'permission'], ['ambient','mention','direct_mention','direct_message'], function (bot, message) {
   bot.reply(message, 'Users should not have access to do things. Neither should devs. Or support staff. \n Permission denied.')
 })
 
-controller.hears(['sql'], ['message_received'], function (bot, message) {
+controller.hears(['sql'], ['ambient','mention','direct_mention','direct_message'], function (bot, message) {
   bot.reply(message, 'SQL should not be used by anyone. Doing so could cause you to change things. This is not allowed.')
 })
 
