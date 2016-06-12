fccdaebcbfdca4f9fd537cac9198e6980089c970
Jon Larman <jlarman@ethosgroup.com>
2016-06-12 10:47:19 -0500

    tweaked a few responses and tried to fix a bug with convo

--- index.js
+++ index.js
@@ -20,25 +20,25 @@ if (token) {
 
     console.log('Connected to Slack RTM')
   })
-// Otherwise assume multi-team mode - setup beep boop resourcer connection
+  // Otherwise assume multi-team mode - setup beep boop resourcer connection
 } else {
   console.log('Starting in Beep Boop multi-team mode')
   require('beepboop-botkit').start(controller, { debug: true })
 }
 
-controller.on('bot_channel_join', function (bot, message) {
-   var welcome = 'AuditBot, roll out! \n' +
-      'I\'m your friendly neighborhood auditbot.\n' +
-      'My job is to make your life easier, by reminding you of all the things you need to do, but can\'t.\n' +
-      'Together, we\'ll achieve a work-free workplace!\n' +
-  bot.reply(message, welcome)
+controller.on(['bot_channel_join', 'bot_group_join'], function (bot, message) {
+  var welcome = 'AuditBot, roll out! \n' +
+    'I\'m your friendly neighborhood auditbot.\n' +
+    'My job is to make your life easier, by reminding you of all the things you need to do, but can\'t.\n' +
+    'Together, we\'ll achieve a work-free workplace!\n' +
+    bot.reply(message, welcome)
 })
 
-controller.hears(['hello', 'hi', 'hey'], ['direct_mention'], function (bot, message) {
+controller.hears(['hello', 'hi', 'hey', 'howdy'], ['direct_mention'], function (bot, message) {
   bot.reply(message, 'Hello. How can I audit you today?')
 })
 
-controller.hears(['hello', 'hi', 'hey'], ['direct_message'], function (bot, message) {
+controller.hears(['hello', 'hi', 'hey', 'howdy'], ['direct_message'], function (bot, message) {
   bot.reply(message, 'Hello.')
   bot.reply(message, 'Ready to enforce compliance.')
 })
@@ -51,41 +51,41 @@ controller.hears('auditbot', ['ambient'], function (bot, message) {
   bot.reply(message, 'AuditBot is always listening. And watching.')
 })
 
-controller.hears(['permissions', 'permission'], ['ambient','mention','direct_mention','direct_message'], function (bot, message) {
+controller.hears(['permissions', 'permission'], ['ambient', 'mention', 'direct_mention', 'direct_message'], function (bot, message) {
   bot.reply(message, 'Users should not have access to do things. Neither should devs. Or support staff. \n Permission denied.')
 })
 
-controller.hears(['sql'], ['ambient','mention','direct_mention','direct_message'], function (bot, message) {
+controller.hears(['sql'], ['ambient', 'mention', 'direct_mention', 'direct_message'], function (bot, message) {
   bot.reply(message, 'SQL should not be used by anyone. Doing so could cause you to change things. This is not allowed.')
 })
 
-controller.hears(['audit something'], 'message_recieved', function(bot,message) {
-    askTopic = function(response, convo) {
-      convo.ask('What would you like me to audit?', function(response, convo) {
-        convo.say('Okey dokey. You asked me to audit ' + response + '.');
-        askHowMuch(response, convo);
-        convo.next();
-      });
-    }
-    askHowMuch = function(response, convo) {
-      convo.ask('How much should I audit ' + response + '?', function(response, convo) {
-        convo.say('Ok. You want me to audit it this much: ' + response + '.')
-        askWhen(response, convo);
-        convo.next();
-      });
-    }
-    askWhen = function(response, convo) {
-      convo.ask('So when should I start?',[
+controller.hears(['audit something'], 'message_recieved', function (bot, message) {
+  askTopic = function (response, convo) {
+    convo.ask('Greetings, <@' + message.user + '>. What would you like me to audit?', function (response, convo) {
+      convo.say('Okey dokey. You asked me to audit ' + response + '.');
+      askHowMuch(response, convo);
+      convo.next();
+    });
+  }
+  askHowMuch = function (response, convo) {
+    convo.ask('How much should I audit ' + response + '?', function (response, convo) {
+      convo.say('Ok. You want me to audit it this much: ' + response + '.')
+      askWhen(response, convo);
+      convo.next();
+    });
+  }
+  askWhen = function (response, convo) {
+    convo.ask('So when should I start?', [
       {
-        pattern: ['now','right away'],
-        callback: function(response,convo) {
+        pattern: ['now', 'right away'],
+        callback: function (response, convo) {
           convo.say('Ok, I\'ll get started now.');
           convo.next();
         }
       },
       {
-        pattern: ['later','next week','tomorrow'],
-        callback: function(response,convo) {
+        pattern: ['later', 'next week', 'tomorrow'],
+        callback: function (response, convo) {
           convo.say('Ok, I\'ll get started later then.');
           // do something else...
           convo.next();
@@ -93,8 +93,8 @@ controller.hears(['audit something'], 'message_recieved', function(bot,message) 
         }
       },
       {
-        pattern: [bot.utterances.no,'never'],
-        callback: function(response,convo) {
+        pattern: [bot.utterances.no, 'never'],
+        callback: function (response, convo) {
           convo.say('Ah man, you got me all excited.');
           // do something else...
           convo.next();
@@ -102,24 +102,24 @@ controller.hears(['audit something'], 'message_recieved', function(bot,message) 
       },
       {
         default: true,
-        callback: function(response,convo) {
+        callback: function (response, convo) {
           // just repeat the question
           convo.repeat();
           convo.next();
         }
       }
     ]);
-    }
+  }
 
-    bot.startConversation(message, askTopic);
-});
+  bot.startConversation(message, askTopic);
+})
 
 controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
   var help = 'I will respond to the following messages: \n' +
-      '`bot hi` for a simple message.\n' +
-      '`bot attachment` to see a Slack attachment message.\n' +
-      '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
-      '`bot help` to see this again.'
+    '`bot hi` for a simple message.\n' +
+    '`bot attachment` to see a Slack attachment message.\n' +
+    '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
+    '`bot help` to see this again.'
   bot.reply(message, help)
 })
 
